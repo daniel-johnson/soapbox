f24d918fe2a5758cee3a8922a81d2a5459319c25
daniel-johnson <janiel.dohnson@gmail.com>
2015-10-13 01:47:42 -0700

    Added homepage route, fixed sources routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'home#index'
-  resources :opinions
-  resources :sourcesb
+  root 'opinions#index'
+  resources :opinions do
+    resources :sources
+  end
+  get '/home', to: 'home#index'
 end
