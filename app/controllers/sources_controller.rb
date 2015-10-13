b7ee6df5beb341ab8f1f7ccb37a4a6af93cfdf66
daniel-johnson <janiel.dohnson@gmail.com>
2015-10-13 01:45:28 -0700

    Worked on the sources controller actions

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -1,10 +1,23 @@
 class SourcesController < ApplicationController
   def create
+    source = Source.create(source_params)
+    flash[:notice] = 'FYI, source added.'
+    redirect_to opinion_path(source.opinion)
   end
 
   def update
+    source = Source.find params[:id]
+    source.update source_params
+    redirect_to source_path(source)
   end
 
   def destroy
+
+  end
+
+  private
+
+  def source_params
+    params.require(:source).permit(:content, :type)
   end
 end
